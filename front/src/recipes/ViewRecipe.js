40bf9005ea6ae84dca96770622e04f8866ca0d87
mihakitak <kitakmiha1@gmail.com>
2025-12-06 23:25:54 +0100

    nalaganje pdf

--- front/src/recipes/ViewRecipe.js
+++ front/src/recipes/ViewRecipe.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import { jsPDF } from "jspdf";
 
 export default function ViewRecipe() {
   const [recipe, setRecipe] = useState({
@@ -9,18 +10,40 @@ export default function ViewRecipe() {
     instructions: "",
   });
 
-  const {id} = useParams();
+const {id} = useParams();
 
   useEffect(() => {
     loadRecipe();
   }, []);
 
-  const loadRecipe = async () => {
-    const result = await axios.get(`http://localhost:8081/recipe/${id}`);
-    setRecipe(result.data);
+const loadRecipe = async () => {
+  const result = await axios.get(`http://localhost:8081/recipe/${id}`);
+  setRecipe(result.data);
   };
 
-  return (
+const downloadPDF = () => {
+  const doc = new jsPDF();
+  const pageWidth = doc.internal.pageSize.getWidth();
+  const margin = 20;
+  const maxWidth = pageWidth - margin * 2;
+
+  doc.setFontSize(18);
+  doc.text(recipe.name, margin, 20);
+
+  doc.setFontSize(14);
+  doc.text("Ingredients:", margin, 40);
+  const ingredientsLines = doc.splitTextToSize(recipe.ingredients, maxWidth - 5);
+  doc.text(ingredientsLines, margin + 5, 50);
+
+  const instructionsStartY = 50 + ingredientsLines.length * 7; 
+  doc.text("Instructions:", margin, instructionsStartY + 10);
+  const instructionsLines = doc.splitTextToSize(recipe.instructions, maxWidth - 5);
+  doc.text(instructionsLines, margin + 5, instructionsStartY + 20);
+
+  doc.save(`${recipe.name}.pdf`);
+};
+
+return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
@@ -42,6 +65,9 @@ export default function ViewRecipe() {
               </ul>
             </div>
           </div>
+          <button className="btn btn-success my-2" onClick={downloadPDF}> 
+            Download PDF 
+          </button>
 
           <Link className="btn btn-primary my-2" to={"/"}>
             Back to Home
